3d5323be3bcc8a52b27118b0687e4310bb89fee8
kiranshelar3 <kiranshelar407@gmail.com>
2022-03-21 10:17:47 +0530

    first commit

---
+++ operation.rb
@@ -0,0 +1,84 @@
+class Operation
+    
+    def initialize(num)
+        @num = num
+    end 
+    
+    def reverse_method
+        reverse = 0
+        while @num >0
+        reverse = reverse*10
+        reverse = reverse + (@num %10)
+        @num = @num/10
+        end
+        return reverse
+    end
+
+    def palindrom
+        # puts "#{reverse_method}"
+        # c ="#{reverse_method}"
+        
+        temp = @num                
+        rev = 0
+        while @num !=0
+            reminder = @num %10
+            @num = @num/10
+            rev= rev*10+reminder
+        end
+
+        # if (temp == c)
+        #     flag = true
+        # end
+        # if flag
+        #     puts "number #{temp} is palindrom"
+        # else
+        #     puts "number #{temp} is not palindrom"
+        # end
+
+        if (temp == rev)
+
+            puts "Number is palindrom"
+        else
+            puts "number is not palindrom"
+        end
+    
+    end
+
+ def prime_or_not
+
+        if @num > 1
+            count = 0
+            for i in 2..@num/2
+            if @num % i == 0
+               count = count + 1
+            end
+
+            end    
+            if count != 0
+                puts "number is not prime"
+            else
+                puts "Number is prime"
+            end
+        end
+    end
+
+    def addition
+        sum = 0
+        while @num !=0
+            reminder = @num %10
+            @num = @num/10
+            sum= sum + reminder
+        end
+        return sum
+
+    end
+end
+
+
+test = Operation.new(922)
+# puts test.reverse_method
+puts test.addition
+# puts test.palindrom
+# put test.addition
+# puts test.prime_or_not
+
